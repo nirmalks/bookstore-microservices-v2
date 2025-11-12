e195f0e006a47176dc2d87eb9dcd4b5139b8e067
nirmalks <kagosekar@gmail.com>
2025-11-12 19:37:50 +0530

    Fix checkoutservice tests and add h2 test dependency for catalog and checkout

--- checkout-service/src/test/java/com/nirmalks/checkout_service/CheckoutServiceApplicationTests.java
+++ checkout-service/src/test/java/com/nirmalks/checkout_service/CheckoutServiceApplicationTests.java
@@ -3,7 +3,13 @@ package com.nirmalks.checkout_service;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+		"user-service.base-url=http://localhost:8081",
+		"catalog-service.base-url=http://localhost:8082",
+		"rabbitmq.exchange.name=bookstore.exchange",
+		"rabbitmq.routing.key=order.created",
+		"rabbitmq.queue.name=order.queue"
+})
 class CheckoutServiceApplicationTests {
 
 	@Test
