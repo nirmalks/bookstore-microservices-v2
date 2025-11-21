2349946965cc21d02ffebe58371646f8d3b8dc7e
nirmalks <4947233+nirmalks@users.noreply.github.com>
2025-11-21 17:02:47 +0530

    Replace old gateway with reactive gateway and modify props. Add debug log for gateway and remove unused logs and imports

--- api-gateway/src/main/java/com/nirmalks/bookstore/api_gateway/security/SecurityConfig.java
+++ api-gateway/src/main/java/com/nirmalks/bookstore/api_gateway/security/SecurityConfig.java
@@ -4,14 +4,10 @@ import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-
-import java.time.Duration;
 
 @Configuration
 @EnableWebFluxSecurity
