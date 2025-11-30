16dff8cf859f58465c3d476cb8a98c3246631567
nirmalks <4947233+nirmalks@users.noreply.github.com>
2025-11-30 20:30:12 +0530

    Refactoring- Separate message send calls to a separate publisher in catalog service

---
+++ catalog-service/src/main/java/com/nirmalks/catalog_service/messaging/InventoryEventPublisher.java
@@ -0,0 +1,40 @@
+package com.nirmalks.catalog_service.messaging;
+
+import dto.StockReservationFailedEvent;
+import dto.StockReservationSuccessEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class InventoryEventPublisher {
+    private final Logger logger = LoggerFactory.getLogger(InventoryEventPublisher.class);
+    private final RabbitTemplate rabbitTemplate;
+
+    public InventoryEventPublisher(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    public void publishStockReservedEvent(String orderId) {
+        StockReservationSuccessEvent event = new StockReservationSuccessEvent(orderId);
+        logger.info("Publishing Stock Reserved event for Order ID: {}", orderId);
+
+        rabbitTemplate.convertAndSend(
+                RabbitMqConfig.INVENTORY_EXCHANGE,
+                RabbitMqConfig.STOCK_RESERVATION_SUCCESS_ROUTING_KEY,
+                event
+        );
+    }
+
+    public void publishStockFailedEvent(String orderId, String reason) {
+        StockReservationFailedEvent event = new StockReservationFailedEvent(orderId, reason);
+        logger.error("Publishing Stock Failed event for Order ID: {} with reason: {}", orderId, reason);
+
+        rabbitTemplate.convertAndSend(
+                RabbitMqConfig.INVENTORY_EXCHANGE,
+                RabbitMqConfig.STOCK_RESERVATION_FAILED_ROUTING_KEY,
+                event
+        );
+    }
+}
--- catalog-service/src/main/java/com/nirmalks/catalog_service/messaging/OrderCreatedEventConsumer.java
+++ catalog-service/src/main/java/com/nirmalks/catalog_service/messaging/OrderCreatedEventConsumer.java
@@ -3,12 +3,9 @@ package com.nirmalks.catalog_service.messaging;
 import com.nirmalks.catalog_service.book.service.BookService;
 import com.rabbitmq.client.Channel;
 import dto.OrderMessage;
-import dto.StockReservationFailedEvent;
-import dto.StockReservationSuccessEvent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.AmqpHeaders;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Service;
@@ -16,29 +13,24 @@ import org.springframework.stereotype.Service;
 @Service
 public class OrderCreatedEventConsumer {
     private final Logger logger = LoggerFactory.getLogger(OrderCreatedEventConsumer.class);
-    private final RabbitTemplate rabbitTemplate;
-
     private BookService bookService;
-    OrderCreatedEventConsumer(BookService bookService, RabbitTemplate rabbitTemplate) {
+
+    private final InventoryEventPublisher eventPublisher;
+
+    OrderCreatedEventConsumer(BookService bookService, InventoryEventPublisher eventPublisher) {
         this.bookService = bookService;
-        this.rabbitTemplate = rabbitTemplate;
+        this.eventPublisher = eventPublisher;
     }
     @RabbitListener(queues = RabbitMqConfig.QUEUE_INVENTORY)
     public void consumeOrderCreatedEvent(OrderMessage message, Channel channel, @Header(AmqpHeaders.DELIVERY_TAG) long deliveryTag) {
         logger.info("Received order event to update inventory: {}", message);
         try {
             bookService.updateStock(message);
-            StockReservationSuccessEvent successEvent = new StockReservationSuccessEvent(message.orderId());
-            rabbitTemplate.convertAndSend(RabbitMqConfig.INVENTORY_EXCHANGE, RabbitMqConfig.STOCK_RESERVATION_SUCCESS_ROUTING_KEY, successEvent);
+            eventPublisher.publishStockReservedEvent(message.orderId());
             logger.info("Successfully processed order: {}", message.orderId());
         } catch (Exception e) {
-            var event = new StockReservationFailedEvent(message.orderId(), e.getMessage());
             logger.info("failed to process order: {}", message.orderId());
-            rabbitTemplate.convertAndSend(
-                    RabbitMqConfig.INVENTORY_EXCHANGE,
-                    RabbitMqConfig.STOCK_RESERVATION_FAILED_ROUTING_KEY,
-                    event
-            );
+            eventPublisher.publishStockFailedEvent(message.orderId(), e.getMessage());
         }
 
     }
