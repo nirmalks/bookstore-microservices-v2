cf343e4e3640ea5a2e6c183540a90dcf5c0f105e
nirmalks <4947233+nirmalks@users.noreply.github.com>
2026-03-30 14:51:39 +0530

    feat: Add jacoco for unit test coverage
    - added jacoco for unit test coverage
    - added jacoco in main pom container
    - added coverage report which covers all dependencies
    - added mocks in main tests and test properties to prevent test errors
    - use mvn clean verify -Pcoverage to generate test reports

--- checkout-service/src/test/java/com/nirmalks/checkout_service/CheckoutServiceApplicationTests.java
+++ checkout-service/src/test/java/com/nirmalks/checkout_service/CheckoutServiceApplicationTests.java
@@ -1,12 +1,17 @@
 package com.nirmalks.checkout_service;
 
 import org.junit.jupiter.api.Test;
+import org.redisson.api.RedissonClient;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest(properties = { "user-service.base-url=http://localhost:8081",
 		"catalog-service.base-url=http://localhost:8082", "rabbitmq.exchange.name=bookstore.exchange",
 		"rabbitmq.routing.key=order.created", "rabbitmq.queue.name=order.queue" })
 class CheckoutServiceApplicationTests {
+
+	@MockitoBean
+	private RedissonClient redissonClient;
 
 	@Test
 	void contextLoads() {
